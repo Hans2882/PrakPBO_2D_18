010e5951d8682b248f72a23f02537cd17f4d6600
Gayuh <gayuhhsuh@gmail.com>
2024-09-22 11:18:15 +0700

    Tugas 4_P3

---
+++ P3/TugasAnggota.java
@@ -0,0 +1,38 @@
+package P3;
+
+public class TugasAnggota {
+    private String NIK;
+    private String Nama;
+    private int Limit;
+    private int pinjam;
+
+    public TugasAnggota(String NIK, String Nama, int Limit) {
+        this.NIK = NIK;
+        this.Nama = Nama;
+        this.Limit = Limit;
+        this.pinjam = 0;
+    }
+
+    public String getNama() {
+        return Nama;
+    }
+
+    public int getLimit() {
+        return Limit;
+    }
+
+    public int getJumlahPinjaman() {
+        return pinjam;
+    }
+
+    public void pinjam(int total) {
+        if (pinjam + total > Limit) {
+            System.out.println("Pinjaman melebihi limit");
+        } else {
+            pinjam += total;
+        }
+    }
+    public void angsur(int total) {
+        pinjam -= total;
+    }
+}
---
+++ P3/TugasKoperasiDemo.java
@@ -0,0 +1,29 @@
+package P3;
+
+public class TugasKoperasiDemo {
+    public static void main(String[] args) {
+        TugasAnggota donny = new TugasAnggota("111333444", "Donny", 5000000);
+
+        System.out.println("Nama Anggota: " + donny.getNama());
+        System.out.println("Limit Pinjaman: " + donny.getLimit());
+
+        System.out.println("\nMeminjam uang 10.000.000...");
+        donny.pinjam(10000000);
+
+        System.out.println("Jumlah pinjaman saat ini: " + donny.getJumlahPinjaman());
+        System.out.println("\nMeminjam uang 4.000.000...");
+
+        donny.pinjam(4000000);
+        System.out.println("Jumlah pinjaman saat ini: " + donny.getJumlahPinjaman());
+
+        System.out.println("\nMembayar angsuran 1.000.000");
+        donny.angsur(1000000);
+
+        System.out.println("Jumlah pinjaman saat ini: " + donny.getJumlahPinjaman());
+
+        System.out.println("\nMembayar angsuran 3.000.000");
+        donny.angsur(3000000);
+
+        System.out.println("Jumlah pinjaman saat ini: " + donny.getJumlahPinjaman());
+    }
+}
