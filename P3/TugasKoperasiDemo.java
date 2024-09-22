221d3f01fac7e6ec3eec0f570651a4a95b9c3041
Gayuh <gayuhhsuh@gmail.com>
2024-09-22 11:37:52 +0700

    Tugas 6_P3

--- P3/TugasKoperasiDemo.java
+++ P3/TugasKoperasiDemo.java
@@ -5,28 +5,41 @@ import java.util.Scanner;
 public class TugasKoperasiDemo {
     public static void main(String[] args) {
         Scanner SC1 = new Scanner(System.in);
+        boolean back = false;
+        int menu;
         TugasAnggota donny = new TugasAnggota("111333444", "Donny", 5000000);
 
         System.out.println("Nama Anggota: " + donny.getNama());
         System.out.println("Limit Pinjaman: " + donny.getLimit());
 
-        System.out.println("\nMeminjam uang 10.000.000...");
-        donny.pinjam(10000000);
+        while (back == false) {
 
-        System.out.println("Jumlah pinjaman saat ini: " + donny.getJumlahPinjaman());
-        System.out.println("\nMeminjam uang 4.000.000...");
-
-        donny.pinjam(4000000);
-        System.out.println("Jumlah pinjaman saat ini: " + donny.getJumlahPinjaman());
-
-        System.out.println("\nMembayar angsuran 100.000");
-        donny.angsur(100000);
-
-        System.out.println("Jumlah pinjaman saat ini: " + donny.getJumlahPinjaman());
-
-        System.out.println("\nMembayar angsuran 3.000.000");
-        donny.angsur(3000000);
-
-        System.out.println("Jumlah pinjaman saat ini: " + donny.getJumlahPinjaman());
+            System.out.println("\nMenu:");
+            System.out.println("1. Masukkan jumlah pinjaman");
+            System.out.println("2. Bayar angsuran");
+            System.out.println("3. Jumlah pinjaman");
+            System.out.println("4. Exit");
+            menu = SC1.nextInt();
+            switch (menu) {
+                case 1:
+                    System.out.println("\nMasukkan jumlah pinjaman");
+                    int jumlah = SC1.nextInt();
+                    donny.pinjam(jumlah);
+                    break;
+                case 2:
+                    System.out.println("\nMembayar angsuran");
+                    int angsur = SC1.nextInt();
+                    donny.angsur(angsur);
+                    break;
+                case 3:
+                    System.out.println("\nJumlah pinjaman saat ini: " + donny.getJumlahPinjaman());
+                    break;
+                case 4:
+                    back = true;
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
